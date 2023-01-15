0d0e9059dd9dc2516b5fada550084eb08778a8ed
Fred <frederik.stonge@gmail.com>
2023-01-15 15:03:26 -0500

    Remove web condition

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -22,7 +21,7 @@ import 'repositories/pacings_repository.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: kIsWeb ? HydratedStorage.webStorageDirectory : await getTemporaryDirectory(),
+    storageDirectory: await getTemporaryDirectory(),
   );
   runApp(const MyApp());
 }
