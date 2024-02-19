f476da60c47e3bd22f102893f1f2eb5a30acd737
Frederik St-Onge <frederik.stonge@gmail.com>
2024-02-19 07:02:12 -0500

    Wrap crud operations in transactions

--- lib/repositories/matches_repository.dart
+++ lib/repositories/matches_repository.dart
@@ -14,28 +14,28 @@ class MatchesRepository {
     final id = db.matchModels.autoIncrement();
     final now = DateTime.now();
     final model = entity.copyWith(id: id, createdDate: now, modifiedDate: now);
-    db.matchModels.put(model);
+    await db.writeAsync((isar) => isar.matchModels.put(model));
     return model;
   }
 
   Future<void> delete(int id) async {
     final db = await databaseRepository.database;
-    db.matchModels.delete(id);
+    await db.writeAsync((isar) => isar.matchModels.delete(id));
   }
 
   Future<void> edit(MatchModel entity) async {
     final model = entity.copyWith(modifiedDate: DateTime.now());
     final db = await databaseRepository.database;
-    db.matchModels.put(model);
+    await db.writeAsync((isar) => isar.matchModels.put(model));
   }
 
   Future<MatchModel?> get(int id) async {
     final db = await databaseRepository.database;
-    return db.matchModels.get(id);
+    return await db.matchModels.getAsync(id);
   }
 
   Future<List<MatchModel>> getList(int skip, int take) async {
     final db = await databaseRepository.database;
-    return db.matchModels.where().findAll(offset: skip, limit: take);
+    return await db.matchModels.where().findAllAsync(offset: skip, limit: take);
   }
 }
