5e98ddb47f2a1059dc0fa2d0951f286efe930b79
Frederik St-Onge <frederik.stonge@gmail.com>
2024-02-29 14:53:11 -0500

    Complete pacing view

---
+++ lib/components/form/drop_down_element.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class DropDownElement<T> extends StatelessWidget {
+  final String label;
+  final T value;
+  final List<DropdownMenuItem<T>> items;
+  final FutureOr<void> Function(T value)? onChanged;
+
+  const DropDownElement({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.items,
+    this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 6),
+          child: Text(
+            label,
+            style: Theme.of(context).textTheme.labelMedium,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        DropdownButtonFormField<T>(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(6.0),
+            ),
+          ),
+          value: value,
+          icon: const Icon(Icons.arrow_downward),
+          onChanged: (value) {
+            onChanged?.call(value as T);
+          },
+          items: items,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/pages/pacing/widgets/improvisation_durations.dart
@@ -0,0 +1,135 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../../../components/bottom_sheet_dialog/bottom_sheet_dialog.dart';
+import '../../../components/duration_picker/duration_picker.dart';
+import '../../../extensions/duration_extensions.dart';
+import '../../../l10n/app_localizations.dart';
+
+class ImprovisationDurations extends StatelessWidget {
+  final String label;
+  final List<int> durations;
+  final FutureOr<void> Function(List<int> durations) onChanged;
+
+  const ImprovisationDurations({
+    super.key,
+    required this.label,
+    required this.durations,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 6),
+          child: Text(
+            label,
+            style: Theme.of(context).textTheme.labelMedium,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        ...durations.asMap().entries.map(
+              (d) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ImprovisationDurationItem(
+                        key: ValueKey('${d.key}${d.value}'),
+                        duration: Duration(seconds: d.value),
+                        valueChanged: (value) async {
+                          final newDurations = List<int>.from(durations);
+                          newDurations[d.key] = value.inSeconds;
+                          await onChanged(newDurations);
+                        },
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.add),
+                      onPressed: () async {
+                        final newDurations = List<int>.from(durations);
+                        newDurations.insert(d.key + 1, const Duration(minutes: 2, seconds: 30).inSeconds);
+                        await onChanged(newDurations);
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.remove),
+                      onPressed: durations.length > 1
+                          ? () async {
+                              final newDurations = List<int>.from(durations);
+                              newDurations.removeAt(d.key);
+                              await onChanged(newDurations);
+                            }
+                          : null,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+      ],
+    );
+  }
+}
+
+class ImprovisationDurationItem extends StatefulWidget {
+  final Duration duration;
+  final ValueChanged<Duration> valueChanged;
+
+  const ImprovisationDurationItem({
+    super.key,
+    required this.duration,
+    required this.valueChanged,
+  });
+
+  @override
+  State<ImprovisationDurationItem> createState() => _ImprovisationDurationItemState();
+}
+
+class _ImprovisationDurationItemState extends State<ImprovisationDurationItem> {
+  final _textController = TextEditingController();
+
+  @override
+  void initState() {
+    _textController.text = widget.duration.toImprovDuration();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      readOnly: true,
+      controller: _textController,
+      onTap: () async {
+        final newDuration = await BottomSheetDialog.showDialog<Duration>(
+          context: context,
+          child: DurationPicker(
+            title: S.of(context).duration,
+            initialDuration: widget.duration,
+          ),
+        );
+
+        if (newDuration != null) {
+          _textController.text = newDuration.toImprovDuration();
+          widget.valueChanged(newDuration);
+        }
+      },
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6.0),
+        ),
+      ),
+    );
+  }
+}
