0480b326fbdb7f97ba87abd78dda62c44d7c3a62
Frederik St-Onge <frederik.stonge@gmail.com>
2024-03-11 19:29:37 -0400

    Fix body padding

--- lib/pages/match/match_page_view.dart
+++ lib/pages/match/match_page_view.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../components/actions/loading_icon_button.dart';
 import '../../components/bottom_sheet_dialog/bottom_sheet_dialog.dart';
 import '../../components/sliver_logo_appbar/sliver_logo_appbar.dart';
-import '../../components/sliver_scaffold/sliver_scaffold.dart';
 import '../../l10n/app_localizations.dart';
 import '../match_detail/pacing_detail_page_shell.dart';
 import 'cubits/match_cubit.dart';
@@ -13,6 +12,7 @@ import 'widgets/match_panel.dart';
 import 'widgets/scoreboard.dart';
 
 class MatchPageView extends StatelessWidget {
+  static const scoreboardHeight = 100.0;
   const MatchPageView({super.key});
 
   @override
@@ -24,31 +24,30 @@ class MatchPageView extends StatelessWidget {
             initial: () => const Center(child: CircularProgressIndicator()),
             error: (error) => Center(child: Text(error)),
             success: (match) => MatchPanel(
-              body: SliverScaffold(
-                slivers: [
-                  SliverLogoAppbar(
-                    title: match.name,
-                    actions: [
-                      LoadingIconButton(
-                        onPressed: () async {
-                          await BottomSheetDialog.showDialog(
-                            context: context,
-                            child: MatchDetailPageShell(
-                              match: match,
-                              onConfirm: (match) async {
-                                await context.read<MatchCubit>().edit(match);
-                              },
-                            ),
-                          );
-                        },
-                        tooltip: S.of(context).editMatch,
-                        icon: const Icon(Icons.edit),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              panelHeader: Scoreboard(match: match),
+              slivers: [
+                SliverLogoAppbar(
+                  title: match.name,
+                  actions: [
+                    LoadingIconButton(
+                      onPressed: () async {
+                        await BottomSheetDialog.showDialog(
+                          context: context,
+                          child: MatchDetailPageShell(
+                            match: match,
+                            onConfirm: (match) async {
+                              await context.read<MatchCubit>().edit(match);
+                            },
+                          ),
+                        );
+                      },
+                      tooltip: S.of(context).editMatch,
+                      icon: const Icon(Icons.edit),
+                    ),
+                  ],
+                ),
+                const SliverFillRemaining(child: Placeholder()),
+              ],
+              panelHeader: Scoreboard(match: match, height: scoreboardHeight),
               panelBody: const Expanded(child: Placeholder()),
             ),
           );
