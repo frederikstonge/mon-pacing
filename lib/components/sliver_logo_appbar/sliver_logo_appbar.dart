683767e103b0e3b0ad4ff2bb5faec692fecbfa51
Frederik St-Onge <frederik.stonge@gmail.com>
2024-04-12 07:03:44 -0400

    Remove copyrighted logo

--- lib/components/sliver_logo_appbar/sliver_logo_appbar.dart
+++ lib/components/sliver_logo_appbar/sliver_logo_appbar.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-
-import '../../cubits/settings/settings_cubit.dart';
-import '../../cubits/settings/settings_state.dart';
-import '../../models/theme_type.dart';
 
 class SliverLogoAppbar extends StatelessWidget {
   static const toolbarHeight = kToolbarHeight + 8;
@@ -31,38 +26,30 @@ class SliverLogoAppbar extends StatelessWidget {
               child: SizedBox(
                 height: toolbarHeight,
                 width: logoWidth,
-                child: BlocBuilder<SettingsCubit, SettingsState>(
-                  builder: (context, state) => switch (state.theme) {
-                    ThemeType.lni => Image.asset(
-                        'assets/lni-logo.png',
-                        fit: BoxFit.contain,
-                      ),
-                    _ => Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                              text: 'Mon',
-                              style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                                    color: Theme.of(context).colorScheme.secondary,
-                                    letterSpacing: -2,
-                                  ),
-                              children: [
-                                TextSpan(
-                                  text: 'Pacing',
-                                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                                        color: Theme.of(context).colorScheme.primary,
-                                        fontWeight: FontWeight.bold,
-                                        letterSpacing: -1,
-                                      ),
-                                ),
-                              ],
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: 'Mon',
+                        style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                              color: Theme.of(context).colorScheme.secondary,
+                              letterSpacing: -2,
                             ),
+                        children: [
+                          TextSpan(
+                            text: 'Pacing',
+                            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                                  color: Theme.of(context).colorScheme.primary,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: -1,
+                                ),
                           ),
                         ],
                       ),
-                  },
+                    ),
+                  ],
                 ),
               ),
             ),
