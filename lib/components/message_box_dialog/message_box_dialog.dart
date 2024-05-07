75224fb439b4294c60f099418a0751650677206a
Frederik St-Onge <frederik.stonge@gmail.com>
2024-05-06 20:35:22 -0400

    Create private widget for messagebox content

--- lib/components/message_box_dialog/message_box_dialog.dart
+++ lib/components/message_box_dialog/message_box_dialog.dart
@@ -14,45 +14,62 @@ class MessageBoxDialog {
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
           child: Padding(
             padding: const EdgeInsets.all(32),
-            child: Builder(
-              builder: (context) {
-                return Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      question,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 12),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        LoadingButton.filled(
-                            child: Text(
-                              yesLabel,
-                              style: const TextStyle(fontWeight: FontWeight.bold),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            onPressed: () => Navigator.of(context).pop(true)),
-                        LoadingButton.tonal(
-                            child: Text(
-                              noLabel,
-                              style: const TextStyle(fontWeight: FontWeight.bold),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            onPressed: () => Navigator.of(context).pop(false)),
-                      ],
-                    ),
-                  ],
-                );
-              },
+            child: _MessageBoxContent(
+              question: question,
+              yesLabel: yesLabel,
+              noLabel: noLabel,
             ),
           ),
         ),
       );
+}
+
+class _MessageBoxContent extends StatelessWidget {
+  final String question;
+  final String yesLabel;
+  final String noLabel;
+
+  const _MessageBoxContent({
+    required this.question,
+    required this.yesLabel,
+    required this.noLabel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          question,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 12),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            LoadingButton.filled(
+                child: Text(
+                  yesLabel,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                onPressed: () => Navigator.of(context).pop(true)),
+            LoadingButton.tonal(
+                child: Text(
+                  noLabel,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                onPressed: () => Navigator.of(context).pop(false)),
+          ],
+        ),
+      ],
+    );
+  }
 }
