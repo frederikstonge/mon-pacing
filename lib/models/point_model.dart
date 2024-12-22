74f04dce1446e8cb61c02401e81dbc54bd58901a
Frederik St-Onge <frederik.stonge@gmail.com>
2024-12-22 09:32:05 -0500

    More entities

--- lib/models/point_model.dart
+++ lib/models/point_model.dart
@@ -1,5 +1,4 @@
 import 'package:dart_mappable/dart_mappable.dart';
-import 'package:drift/drift.dart';
 import 'package:isar/isar.dart';
 
 part 'point_model.mapper.dart';
@@ -19,9 +18,4 @@ class PointModel with PointModelMappable {
     required this.improvisationId,
     required this.value,
   });
-
-  static JsonTypeConverter<PointModel, String> converter = TypeConverter.json(
-    fromJson: (json) => PointModelMapper.fromMap(json as Map<String, Object?>),
-    toJson: (pref) => pref.toMap(),
-  );
 }
