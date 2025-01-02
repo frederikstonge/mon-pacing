c455efb9ef8d953af6eb8d91bd6b1a1c83f559ad
Frederik St-Onge <frederik.stonge@gmail.com>
2025-01-01 21:14:26 -0500

    Rename database class

--- lib/repositories/app_database_repository.dart
+++ lib/repositories/app_database_repository.dart
@@ -7,14 +7,14 @@ import 'entities/pacing_entity.dart';
 import 'entities/team_entity.dart';
 import 'legacy_database_repository.dart';
 
-part 'database_repository.g.dart';
+part 'app_database_repository.g.dart';
 
 @DriftDatabase(tables: [PacingEntity, MatchEntity, TeamEntity])
-class AppDatabase extends _$AppDatabase {
+class AppDatabaseRepository extends _$AppDatabaseRepository {
   final LegacyDatabaseRepository legacyDatabaseRepository;
   final ToasterService toasterService;
 
-  AppDatabase({
+  AppDatabaseRepository({
     required this.legacyDatabaseRepository,
     required this.toasterService,
   }) : super(_openConnection());
@@ -26,14 +26,15 @@ class AppDatabase extends _$AppDatabase {
   MigrationStrategy get migration => MigrationStrategy(
         beforeOpen: (details) async {
           await customStatement('PRAGMA foreign_keys = ON');
+        },
+        onCreate: (m) async {
+          await m.createAll();
 
-          if (details.wasCreated) {
-            // Migrate data from the legacy database
-          }
+          // Run migrations from the legacy database
         },
       );
 
   static QueryExecutor _openConnection() {
-    return driftDatabase(name: 'mon_pacing_drift');
+    return driftDatabase(name: 'mon_pacing');
   }
 }
