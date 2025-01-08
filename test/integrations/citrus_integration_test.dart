a1be37349ca21f1c3c8a9d26e18a3e9c666c47e8
Frederik St-Onge <frederik.stonge@gmail.com>
2025-01-08 15:09:06 -0500

    Add tests with static html and online

--- test/integrations/citrus_integration_test.dart
+++ test/integrations/citrus_integration_test.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:html/parser.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
 import 'package:mon_pacing/integrations/citrus_integration.dart';
 import 'package:mon_pacing/models/improvisation_model.dart';
@@ -9,6 +12,8 @@ import 'package:mon_pacing/models/improvisation_type.dart';
 import 'package:mon_pacing/models/pacing_model.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   group('Citrus static data', () {
     const url = 'https://test.com/citrus';
     late CitrusIntegration citrusIntegration;
@@ -95,24 +100,84 @@ void main() {
       );
 
       final html = await File('./test/integrations/static_data.html').readAsString();
-      dioAdapter.onGet(url, (server) => server.reply(200, html, delay: const Duration(seconds: 1)));
+      dioAdapter.onGet(
+        url,
+        (server) => server.reply(
+          200,
+          html,
+          headers: const {
+            Headers.contentTypeHeader: ['text/html']
+          },
+          delay: const Duration(seconds: 1),
+        ),
+      );
 
       final match = await citrusIntegration.getMatch(url, pacing);
       expect(match, isNotNull);
+      expect(match.teams.length, 2);
+      expect(match.teams[0].performers.length, 6);
+      expect(match.teams[1].performers.length, 7);
+      expect(match.integrationId, citrusIntegration.integrationId);
+      expect(match.integrationEntityId, '9292');
+      expect(match.integrationAdditionalData, isNotNull);
+      final exportData = jsonDecode(match.integrationAdditionalData!);
+      expect(exportData['csrfToken'], '1mIBje6wUiC6ALqMl9EazrEyZlRtJTcpXhpOykU6iKHEb7jA8OgTj9zJ68pnvhNU');
     });
   });
 
-  // group('Citrus web data', () {
-  //   late CitrusIntegration citrusIntegration;
-  //   final url = 'https://test.com/citrus';
+  group('Citrus web data', () {
+    late CitrusIntegration citrusIntegration;
+    late Dio dio;
+    final url = 'https://citrus.liguedespamplemousses.com/Citrus/Match-0379f292b8db8d7a6e560a2e62410084d3abf74a2775d026a0524a17afc47014/';
 
-  //   setUp(() {
-  //     final dio = Dio(BaseOptions());
-  //     citrusIntegration = CitrusIntegration(client: dio);
-  //   });
+    setUp(() {
+      dio = Dio();
+      citrusIntegration = CitrusIntegration(client: dio);
+    });
 
-  //   test('integrationIsValid should return true', () {
-  //     expect(citrusIntegration!.integrationIsValid(url), isTrue);
-  //   });
-  // });
+    test('Html body is the same', () async {
+      final html = File('./test/integrations/static_data.html').readAsStringSync();
+      final staticDocument = parse(html);
+
+      final response = await dio.get<String>(url.toString());
+      final document = parse(response.data);
+
+      expect(document.body!.text, staticDocument.body!.text);
+    });
+
+    test('integrationIsValid should return true', () {
+      expect(citrusIntegration.integrationIsValid(url), isTrue);
+    });
+
+    test('getMatch should return match when data is good', () async {
+      final pacing = PacingModel(
+        id: 1,
+        name: 'Pacing',
+        createdDate: DateTime.now(),
+        modifiedDate: DateTime.now(),
+        improvisations: List.generate(
+          12,
+          (index) => ImprovisationModel(
+            id: index,
+            type: ImprovisationType.values.elementAt(index % 2),
+            category: '',
+            theme: '',
+            durationsInSeconds: [Duration(minutes: 2).inSeconds],
+            performers: '',
+            notes: '',
+          ),
+        ),
+      );
+
+      final match = await citrusIntegration.getMatch(url, pacing);
+      expect(match, isNotNull);
+      expect(match.teams.length, 2);
+      expect(match.teams[0].performers.length, 6);
+      expect(match.teams[1].performers.length, 7);
+      expect(match.integrationId, citrusIntegration.integrationId);
+      expect(match.integrationEntityId, '9292');
+      final exportData = jsonDecode(match.integrationAdditionalData!);
+      expect(exportData['csrfToken'], isNotNull);
+    });
+  });
 }
