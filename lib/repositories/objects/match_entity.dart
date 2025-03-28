20be4f466328375cc50b5bb7818bc686afc71ec0
Frederik St-Onge <frederik.stonge@gmail.com>
2025-03-28 14:07:27 -0400

    Update objects constructors

--- lib/repositories/objects/match_entity.dart
+++ lib/repositories/objects/match_entity.dart
@@ -8,29 +8,29 @@ import 'star_entity.dart';
 
 @Entity()
 class MatchEntity {
-  late int id;
+  int id;
   @Index()
-  late String name;
-  @Index()
-  @Property(type: PropertyType.date)
-  late DateTime? createdDate;
+  String name;
   @Index()
   @Property(type: PropertyType.date)
-  late DateTime? modifiedDate;
-  late List<String> tags;
-  late bool enableStatistics;
-  late bool enablePenaltiesImpactPoints;
-  late int penaltiesImpactType;
-  late int penaltiesRequiredToImpactPoints;
-  late bool enableMatchExpulsion;
-  late int penaltiesRequiredToExpel;
-  late String? integrationId;
-  late String? integrationEntityId;
-  late String? integrationAdditionalData;
-  late int? integrationRestrictMaximumPointPerImprovisation;
-  late int? integrationMinNumberOfImprovisations;
-  late int? integrationMaxNumberOfImprovisations;
-  late List<String>? integrationPenaltyTypes;
+  DateTime? createdDate;
+  @Index()
+  @Property(type: PropertyType.date)
+  DateTime? modifiedDate;
+  List<String> tags;
+  bool enableStatistics;
+  bool enablePenaltiesImpactPoints;
+  int penaltiesImpactType;
+  int penaltiesRequiredToImpactPoints;
+  bool enableMatchExpulsion;
+  int penaltiesRequiredToExpel;
+  String? integrationId;
+  String? integrationEntityId;
+  String? integrationAdditionalData;
+  int? integrationRestrictMaximumPointPerImprovisation;
+  int? integrationMinNumberOfImprovisations;
+  int? integrationMaxNumberOfImprovisations;
+  List<String>? integrationPenaltyTypes;
 
   final teams = ToMany<MatchTeamEntity>();
   final improvisations = ToMany<ImprovisationEntity>();
@@ -38,7 +38,26 @@ class MatchEntity {
   final points = ToMany<PointEntity>();
   final stars = ToMany<StarEntity>();
 
-  MatchEntity();
+  MatchEntity({
+    this.id = 0,
+    required this.name,
+    required this.enableMatchExpulsion,
+    required this.enablePenaltiesImpactPoints,
+    required this.enableStatistics,
+    required this.penaltiesImpactType,
+    required this.penaltiesRequiredToExpel,
+    required this.penaltiesRequiredToImpactPoints,
+    this.createdDate,
+    this.modifiedDate,
+    this.tags = const [],
+    this.integrationId,
+    this.integrationEntityId,
+    this.integrationAdditionalData,
+    this.integrationRestrictMaximumPointPerImprovisation,
+    this.integrationMinNumberOfImprovisations,
+    this.integrationMaxNumberOfImprovisations,
+    this.integrationPenaltyTypes,
+  });
 
   List<String> get teamNames => teams.map((e) => e.name).toList();
 }
