2f7f56aec6441f451144acb3d099833bbacf546d
Frederik St-Onge <frederik.stonge@gmail.com>
2025-08-10 15:06:03 -0400

    Add scanner logic

--- lib/integrations/real_time_match_integration_base.dart
+++ lib/integrations/real_time_match_integration_base.dart
@@ -3,9 +3,10 @@ import 'dart:async';
 import '../models/integration_base_model.dart';
 import '../models/match_model.dart';
 import '../models/timer_model.dart';
-import 'match_integration_base.dart';
+import 'integration_base.dart';
 
-abstract class RealTimeMatchIntegrationBase extends MatchIntegrationBase {
+abstract class RealTimeMatchIntegrationBase extends IntegrationBase {
+  FutureOr<MatchModel> enrichMatch(String scanData, MatchModel match);
   FutureOr<MatchModel> onMatchUpdate(MatchModel match);
   FutureOr<bool> onTimerUpdate(IntegrationBaseModel integration, TimerModel? timer);
 }
